6a1b9443a4f9d74bd9c0a8fea9ec6849febabd8f
a-kit-2 <akito.nakamura.c@gmail.com>
2021-12-13 18:18:37 +0900

    showメソッド作成

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -1,11 +1,35 @@
 class UsersController < ApplicationController
+  # GET /users
   def index
     @users = User.all
     render json: { users: @users }
   end
 
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    render json: { user: @user }
+  end
+
+  # POST /users
   def create
-    @user = User.new
+    @user = User.new(users_params)
     @user.save
+  end
+
+  # PATCH/PUT /users/:id
+  def update
+    
+  end
+
+  # DELETE /users/:id
+  def destroy
+    
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:name, :email)
   end
 end
