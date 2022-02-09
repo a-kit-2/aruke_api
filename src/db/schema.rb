93c765354cc1aa8793574814642452cada32f1a9
a-kit-2 <akito.nakamura.c@gmail.com>
2022-02-09 18:22:50 +0900

    auth0_idを追加

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_082703) do
+ActiveRecord::Schema.define(version: 2022_02_09_091555) do
 
   create_table "goals", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "steps"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_12_13_082703) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "is_deleted"
+    t.bigint "auth0_id"
   end
 
   add_foreign_key "goals", "users"
