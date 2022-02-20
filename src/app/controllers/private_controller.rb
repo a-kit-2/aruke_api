3ef4ffa7f7640f0d39a1fd36047799dbecaa14b1
a-kit-2 <akito.nakamura.c@gmail.com>
2022-02-20 15:32:32 +0900

    テスト用パラメータ追加

--- src/app/controllers/private_controller.rb
+++ src/app/controllers/private_controller.rb
@@ -3,7 +3,7 @@ class PrivateController < ActionController::API
   include Secured
 
   def private
-    render json: { message: 'Hello from a private endpoint! You need to be authenticated to see this.' }
+    render json: { message: 'Hello from a private endpoint! You need to be authenticated to see this.', auth_payload: @auth_payload, auth_header: @auth_header }
   end
 
   def private_scoped
