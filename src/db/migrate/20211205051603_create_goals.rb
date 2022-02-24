ccb1367870435b2d468ff0e4754de93a5ea829c5
a-kit-2 <akito.nakamura.c@gmail.com>
2022-02-24 15:13:55 +0900

    migrationファイル修正

--- src/db/migrate/20211205051603_create_goals.rb
+++ src/db/migrate/20211205051603_create_goals.rb
@@ -5,7 +5,6 @@ class CreateGoals < ActiveRecord::Migration[6.1]
       t.datetime :term
       t.integer :penalties
       t.boolean :is_achieved
-      t.boolean :is_deleted
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
